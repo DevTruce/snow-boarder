e9b9b3b5f61240ff45bfd8f8dcd17048afb7da5d
Truce Ramcharitar <DevTruce@Gmail.com>
2023-10-04 21:07:31 -0400

    setup particle effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,10 +7,12 @@ public class CrashDetector : MonoBehaviour
 {
 
    [SerializeField] float reloadSceneDelay = 0.5f;
+   [SerializeField] ParticleSystem crashEffect;
    private void OnTriggerEnter2D(Collider2D other) 
    {
         if(other.tag == "Ground")
         {
+           crashEffect.Play();
            Invoke("ReloadScene", reloadSceneDelay);
         }
    }
