3800942a55d23e42a067657b5c8c7b417b2515b9
Truce Ramcharitar <DevTruce@Gmail.com>
2023-10-04 21:45:51 -0400

    created ability to boost

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-[SerializeField] float torqueAmount = 1f;
+    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+    SurfaceEffector2D surfaceEffector2D;
     Rigidbody2D rb2d;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-       rb2d = GetComponent<Rigidbody2D>();
-    }
 
-    // Update is called once per frame
-    void Update()
+   void RotatePlayer()
     {
-       if(Input.GetKey(KeyCode.LeftArrow))
+      if(Input.GetKey(KeyCode.LeftArrow))
        {
         rb2d.AddTorque(torqueAmount);
        }
@@ -25,4 +23,33 @@ public class PlayerController : MonoBehaviour
         rb2d.AddTorque(-torqueAmount);
        }
     }
+
+     void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+         surfaceEffector2D.speed = boostSpeed;
+        } 
+        else 
+        {
+         surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb2d = GetComponent<Rigidbody2D>();
+       surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       RotatePlayer();
+       RespondToBoost();
+    }
+
 }
